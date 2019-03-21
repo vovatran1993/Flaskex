1d2a3e939dade19905e29ee716a0b09631677cbd
vovatran1993 <vovatran1993@gmail.com>
2019-03-20 19:59:14 -0500

    new

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,14 +1,11 @@
-node {
+{
 properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give to build IP', name: 'IP', trim: true)])])
    
     stage("Install git"){
         sh "ssh    ec2-user@${IP}   sudo yum install git python-pip -y"
     }
     stage("Clone a repo"){
-        git 'https://github.com/vovatran1993/Flaskex.git'
-    }
-    stage("Copy files"){
-        sh "scp  -r * ec2-user@${IP}:/tmp/"
+        git 'https://github.com/sstanytska/Flaskex.git'
     }
     stage("Install requirements"){
         sh "ssh ec2-user@${IP}   sudo pip install -r /tmp/requirements.txt"
@@ -16,5 +13,16 @@ properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please g
     stage("Run App"){
         sh "ssh ec2-user@${IP}    python /tmp/app.py"
     }
-
+    stage("Copy files"){
+        sh "scp  -r * ec2-user@${IP}:/tmp/"
+    }
+    stage("Move files to /flesk"){
+        sh "ssh ec2-user@${IP}      sudo pip install -r /klask/requirements.txt"
+    }
+    stage("Move service to /etc"){
+        sh "ssh ec2-user@${IP}      sudo /flaskex/flaskex.service /etc/system/system"
+    }
+    stage("Start service"){
+        sh "ssh ssh ec2-user@${IP} sudo systemctl start flaskex"
+    }
 }
