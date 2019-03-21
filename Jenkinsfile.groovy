9e2d0d9c4a6aa26deca934929de5222da6ba9a0f
vovatran1993 <vovatran1993@gmail.com>
2019-03-20 19:20:53 -0500

    git fixed

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please g
         sh "ssh    ec2-user@${IP}   sudo yum install git python-pip -y"
     }
     stage("Clone a repo"){
-        git 'git@github.com:vovatran1993/Flaskex.git'
+        git 'https://github.com/vovatran1993/Flaskex.git'
     }
     stage("Copy files"){
         sh "scp  -r * ec2-user@${IP}:/tmp/"
