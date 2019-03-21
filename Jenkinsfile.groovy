5d965d9bdf3e8f0327a692f61734828e8aa9c3b1
vovatran1993 <vovatran1993@gmail.com>
2019-03-20 19:18:18 -0500

    flaskes

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+node {
+properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give to build IP', name: 'IP', trim: true)])])
+   
+    stage("Install git"){
+        sh "ssh    ec2-user@${IP}   sudo yum install git python-pip -y"
+    }
+    stage("Clone a repo"){
+        git 'git@github.com:vovatran1993/Flaskex.git'
+    }
+    stage("Copy files"){
+        sh "scp  -r * ec2-user@${IP}:/tmp/"
+    }
+    stage("Install requirements"){
+        sh "ssh ec2-user@${IP}   sudo pip install -r /tmp/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh ec2-user@${IP}  python  python /tmp/app.py"
+    }
+
+}
